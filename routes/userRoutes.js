e2ef9142eade411042fe426ecdc242f93920205d
JuanSalda91 <juansalda9131@gmail.com>
2026-01-21 15:26:56 -0500

    added POST(login) route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const User = require('../models/User')
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
@@ -8,6 +9,7 @@ const router = express.Router();
 //     res.json({ message: "user routes working!!!" });
 // });
 
+// POST/api/users/register
 router.post('/register', async (req, res) => {
     try {
       const { username, email, password } = req.body;
@@ -47,5 +49,55 @@ router.post('/register', async (req, res) => {
       return res.status(500).json({ message: 'Server error' });
     }
   });
+
+// POST/api/users/login
+router.post('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        //basic validation
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Please provide email and password.' });
+        }
+
+        //find user by email
+        const user = await User.findOne({ email });
+
+        if(!user) {
+            return res.status(400).json({ message: 'Incorrect email or password.' });
+        }
+
+        //check password using instance method
+        const isMatch = await user.isCorrectPassword(password);
+
+        if(!isMatch) {
+            return res.status(400).json({ message: 'Incorrect email or password' });
+        }
+
+        // create JWT payload
+        const payload = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+        };
+
+        const token = jwt.sign(payload, process.env.JWT_SECRET, {
+            expiresIn: '1h',
+        });
+
+        //prepare user data withput password
+        const userResponse = {
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            createdAt: user.createdAt,
+        };
+
+        return res.json({ token, user: userResponse });
+    } catch (err) {
+        console.error('Error in /login:', err);
+        return res.status(500).json({ message: 'server error' });
+    }
+});
 
 module.exports = router;
